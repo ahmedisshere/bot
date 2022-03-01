2d95b5221440ba2a673f65db126d85981661954a
Tausif Ahmed <tausifahmed471@gmail.com>
2022-03-01 23:26:59 +0600

    chatbot initiated

---
+++ static/scripts/responses.js
@@ -0,0 +1,36 @@
+
+function getBotResponse(input) {
+
+    //rock paper scissors
+    if (input == "rock") {
+        return "paper";
+    } 
+    
+    else if (input == "paper") {
+        return "scissors";
+    } 
+
+    else if (input == "scissors") {
+        return "rock";
+    }
+
+
+
+    // Simple responses
+
+
+
+    if (input == "hello") {
+        return "Hello there!";
+    } 
+    
+    
+    else if (input == "goodbye") {
+        return "Where are you heading?!";
+    } 
+    
+    
+    else {
+        return "Try asking something else!";
+    }
+}
